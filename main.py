a54b5661b77b9c7e8c09581c5b089583ca8a7283
Nnamaka001 <nnamaka.azubuike@yahoo.com>
2022-11-12 06:19:08 +0100

    Third Commit

--- main.py
+++ main.py
@@ -63,8 +63,8 @@ plt.show()'''
 '''from numpy import random
 import matplotlib.pyplot as plt
 import seaborn as sns
-sns.distplot(random.binomial(n = 100, p = 0.5, size = 750), label = "binomial")
-sns.distplot(random.normal(loc=100, scale=0.75, size = 750), label = "normal")
+sns.distplot(random.normal(loc=50, scale=5, size=1000), hist=False, label='normal')
+sns.distplot(random.binomial(n=100, p=0.5, size=1000), hist=False, label='binomial')
 plt.show()'''
 
 '''from numpy import random
@@ -91,3 +91,27 @@ sns.set_theme()
 df = sns.load_dataset("penguins")
 sns.distplot(df["flipper_length_mm"])
 plt.show()'''
+
+#Create a 2x3 uniform distribution sample
+'''from numpy import random
+x = random.uniform(size=(2, 3))
+print(x)'''
+
+'''from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.distplot(random.uniform(size=1000), hist=False)
+plt.show()'''
+
+# Draw out a sample for dice roll (multinominal distribution is used)
+'''from numpy import random
+x = random.multinomial(n=6, pvals=[1/6, 1/6, 1/6, 1/6, 1/6, 1/6])
+print(x)'''
+
+'''from numpy import random  #For multinomial VISUALIZATION
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.distplot(random.multinomial(n=6, pvals=[1/6,1/6,1/6,1/6,1/6,1/6], size = 1000), hist = False)
+plt.show()'''
+
+
