8557a901a3c9ceaae1c9a746aebb9366581b9b76
Nnamaka001 <nnamaka.azubuike@yahoo.com>
2022-11-18 06:14:58 +0100

    third commit

--- main.py
+++ main.py
@@ -114,4 +114,112 @@ import seaborn as sns
 sns.distplot(random.multinomial(n=6, pvals=[1/6,1/6,1/6,1/6,1/6,1/6], size = 1000), hist = False)
 plt.show()'''
 
+#Draw out a sample for exponential distribution with 2.0 scale with 2x3 size:
 
+'''from numpy import random
+x = random.exponential(scale=2, size=(2,3))
+print(x)'''
+
+'''from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.distplot(random.exponential(size=1000), hist =  False)
+plt.show()'''
+
+#Draw out a sample for a chi squared distribution with degree of freedom 2, size 2x3:
+
+'''from numpy import random
+x = random.chisquare(df=2, size=(2,3))
+print(x)'''
+
+'''from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.distplot(random.chisquare(df=2, size=1000), hist = False)
+plt.show()'''
+
+#VISUALIZATION FOR PARETO DISTRIBUTION
+
+'''from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.displot(random.pareto(a=2, size=1000), kde = False)
+plt.show()'''
+
+'''x = [1, 2, 3, 4]
+y = [4, 5, 6, 7]
+z = []
+for i, j in zip(x, y):
+    z.append(i + j)
+    print(z)'''
+
+'''import numpy as np
+x = [1, 2, 3, 4]
+y = [4, 5, 6, 7]
+z = np.add(x, y)
+print(z)'''
+
+'''import numpy as np
+arr = np.array([1, 2, 3, 4])
+x = np.prod(arr)
+print(x)'''
+
+'''import numpy as np
+def myadd(x, y):
+    return x + y
+
+myadd = np.frompyfunc(myadd, 2, 1)
+print(myadd([1, 2, 3, 4], [5, 6, 7, 8]))'''
+
+#LCM for individual numbers
+'''import numpy as np
+num1 = 4
+num2 = 6
+x = np.lcm(num1, num2)
+print(x)'''
+
+#LCM for arrays
+'''import numpy as np
+arr = np.array([3, 6, 9])
+x = np.lcm.reduce(arr)
+print(x)'''
+
+#Find LCM of all values of an array where the array contains integers from 1 to 10
+'''import numpy as np
+arr = np.arange(1, 11) #arange() will take integers from 1 to 10. 11 won't be included
+x = np.lcm.reduce(arr)
+print(x)'''
+
+'''import numpy as np
+x = np.sin(np.pi/2)
+print(x)'''
+
+'''import numpy as np
+arr = np.array([90, 180, 270, 360])
+x = np.deg2rad(arr)
+print(x)'''
+
+'''import numpy as np
+arr = np.array([1,1,1,2,3,4,5,5,6,7])
+x = np.unique(arr)
+print(x)'''
+
+'''import numpy as np
+arr1 = np.array([1,2,3,4])
+arr2 = np.array([3,4,5,6])
+newarr = np.union1d(arr1, arr2)
+print(newarr)'''
+
+'''import numpy as np
+arr1 = np.array([1,2,3,4])
+arr2 = np.array([3,4,5,6])
+newarr = np.intersect1d(arr1, arr2)
+print(newarr)'''
+
+'''import numpy as np
+arr1 = np.array([1,2,3,4])
+arr2 = np.array([3,4,5,6])
+newarr = np.setdiff1d(arr1, arr2)
+newarr1 = np.setxor1d(arr1, arr2)
+print(newarr)
+print(newarr1)'''
